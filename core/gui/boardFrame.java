f400795f58a046921965863e5d86d48823bfdaf6
plnyyanks@gmail.com <plnyyanks@gmail.com@94166920-f482-9620-bf44-6b1e3f9a293d>
2012-05-28 02:06:55 +0000

    removed unused import

--- core/gui/boardFrame.java
+++ core/gui/boardFrame.java
@@ -17,7 +17,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import core.game;
-import core.rack;
 import core.tile;
 
 public class boardFrame extends JFrame implements ActionListener{
